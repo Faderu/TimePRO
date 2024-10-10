a54049b4d884b5f31b8215756c9a07d077ada160
Faderu <137371292+Faderu@users.noreply.github.com>
2024-10-10 22:48:17 +0800

    Update __init__.py

--- timepro/__init__.py
+++ timepro/__init__.py
@@ -1,6 +1,6 @@
 from .deadline_tracker import DeadlineStatus, DeadlinePriority, Deadline, DeadlineTracker, DeadlineFileManager
 from .goal_settings import GoalStatus, GoalPriority, Goal, Milestone, GoalTracker, GoalFileManager
-from .habit_tracker import HabitCategory, Habit, HabitTracker
+from .habit_tracker import HabitCategory, Habit, HabitTracker, HabitFileManager
 from .pomodoro_timer import PomodoroTimer
 from .task_management import TaskPriority, TaskStatus, Task, TaskManager, TaskFileManager
 from .time_analysis import TimeCategory, TimeEntry, TimeAnalyzer, TimeFileManager
@@ -22,6 +22,7 @@ __all__ = [
     "HabitCategory",
     "Habit",
     "HabitTracker",
+    "HabitFileManager",
     "PomodoroTimer",
     "TaskPriority",
     "TaskStatus",
